e9fb2b27d15c47c1600c6b1ab67f6d9db0f5412a
JFMerino <jrfm.meri@gmail.com>
2023-07-07 14:30:12 -0500

    Version 1.0, terminada y probada

---
+++ src/data/books.js
@@ -0,0 +1,50 @@
+export const books = [
+	{
+		id: 'clean-code',
+		title: 'Clean Code: Manual de desarrollo ágil de software',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg',
+		author: 'Robert C. Martin',
+		opinion: 'Este libro es una guía esencial para cualquier desarrollador de software. Proporciona principios y prácticas claras para escribir un código limpio y de calidad. Recomiendo encarecidamente su lectura.',
+		urlCompra: 'https://www.amazon.com/gp/product/B001GSTOAM/ref=x_gr_bb_kindle?caller=Goodreads&tag=x_gr_bb_kindle-20'
+	},
+	{
+		id: 'the-pragmatic-programmer',
+		title: 'The Pragmatic Programmer: From Journeyman to Master',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg',
+		author: 'Andrew Hunt y David Thomas',
+		opinion: 'Este libro ofrece consejos prácticos y experiencia acumulada para ayudarte a mejorar tus habilidades como programador. Es una lectura imprescindible para aquellos que desean crecer y convertirse en maestros en su campo.',
+		urlCompra: 'https://www.amazon.com/gp/product/B00A2KOP0E/ref=x_gr_bb_amazon?ie=UTF8&tag=x_gr_bb_amazon-20&linkCode=as2&camp=1789&creative=9325&creativeASIN=B00A2KOP0E&SubscriptionId=1MGPYB6YW3HWK55XCGG2'
+	},
+	{
+		id: 'cleane-coder',
+		title: 'The Clean Coder: un código de conducta para programadores profesionales',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg',
+		author: 'Robert C. Martin',
+		opinion: 'En este libro, Martin explora el código de conducta y las habilidades necesarias para ser un programador profesional. Ofrece una perspectiva valiosa sobre la ética y la responsabilidad en el desarrollo de software.',
+		urlCompra: 'https://www.amazon.com/gp/product/B0050JLC9Y/ref=x_gr_bb_kindle?caller=Goodreads&tag=x_gr_bb_kindle-20'
+	},
+	{
+		id: 'patrones-de-arquitectura-de-aplicaciones-empresariale',
+		title: 'Patrones de arquitectura de aplicaciones empresariale',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1440294142i/70156.jpg',
+		author: 'Martin Fowler',
+		opinion: 'Este libro es una referencia imprescindible para aquellos que desean comprender y aplicar patrones de arquitectura en el desarrollo de aplicaciones empresariales. Fowler presenta de manera clara y concisa los patrones más comunes y su uso.',
+		urlCompra: 'https://www.amazon.com/gp/product/B008OHVDFM/ref=x_gr_bb_kindle?caller=Goodreads&tag=x_gr_bb_kindle-20'
+	},
+	{
+		id: 'eloquent-javascript',
+		title: 'Eloquent JavaScript: A Modern Introduction to Programming',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1308260856i/8910666.jpg',
+		author: 'Marijn Haverbeke',
+		opinion: 'Este libro es una excelente introducción a la programación utilizando JavaScript. Es fácil de seguir y presenta conceptos complejos de manera accesible. Es ideal para principiantes y también para aquellos que desean mejorar sus habilidades en JavaScript.',
+		urlCompra: 'https://www.amazon.com/gp/product/1593272820/ref=x_gr_bb_amazon?ie=UTF8&tag=x_gr_bb_amazon-20&linkCode=as2&camp=1789&creative=9325&creativeASIN=1593272820&SubscriptionId=1MGPYB6YW3HWK55XCGG2'
+	},
+	{
+		id: 'learning-python',
+		title: 'Learning Python',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1386922968i/80435.jpg',
+		author: 'Mark Lutz',
+		opinion: 'Este libro es una guía completa para aprender Python, uno de los lenguajes de programación más populares. Proporciona ejemplos prácticos y ejercicios para ayudarte a dominar el lenguaje. Es altamente recomendado para principiantes y programadores intermedios de Python.',
+		urlCompra: 'https://www.amazon.com/gp/product/B0043EWVHO/ref=x_gr_bb_kindle?caller=Goodreads&tag=x_gr_bb_kindle-20'
+	}
+]
